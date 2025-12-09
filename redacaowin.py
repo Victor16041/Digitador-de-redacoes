746291be7eae6875b0a2cb00cea45a3792c7c17f
Victor1604 <anonimoestelar1604@gmail.com>
2025-12-09 08:45:28 -0300

    Add files via upload

--- redacaowin.py
+++ redacaowin.py
@@ -2,20 +2,24 @@ import time
 import keyboard
 import tkinter as tk
 from tkinter import simpledialog
+from google.genai import types
 from google import genai
 
 root = tk.Tk()
 root.withdraw()
 
-client = genai.Client(api_key="Coloque sua chave de api do gemini aqui nessas aspas") # coloque sua chave da api do gemini
+client = genai.Client(api_key="Coloque a chave api aqui") # coloque sua chave da api do gemini
 
 # pede pra pessoa digitar o tema
 redacao = simpledialog.askstring("Sua redação MANEIRA", "Digite o tema da sua redação")
 
 # gera o texto da ia
 response = client.models.generate_content(
-    model="gemini-2.0-flash",
-    contents=[redacao]
+    model="gemini-2.5-flash",
+    contents=[redacao],
+    config=types.GenerateContentConfig(
+        system_instruction="O seu proposito vai ser escrever uma redação de no minimo 160 palavras e maximo de 200 palavras sobre o tema que eu pedir. Ela sempre terá que ser em português brasileiro, e de forma totalmente formal."
+    )
 )
 
 # tenta extrair o texto da resposta
